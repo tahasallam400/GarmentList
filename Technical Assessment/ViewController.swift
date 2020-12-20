6ca7a49f31a87cca9dc5c63965d1b13fe70312f2
taha <taha.abdelrahman@mnasat.com>
2020-12-20 16:08:10 -0600

    Initial Commit

---
+++ Technical Assessment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Technical Assessment
+//
+//  Created by TAHA SALLAM on 12/20/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
